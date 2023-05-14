0c65c03d0ef5081b8f6fafba0c7fee7347e7b31c
cans1z <canss1zzz@gmail.com>
2023-05-14 20:41:48 +0300

    Nothing changed

--- BG/Assets/Scripts/TargetHumen.cs
+++ BG/Assets/Scripts/TargetHumen.cs
@@ -1,14 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class TargetHumen : MonoBehaviour
 {
     public static int HP = 1;
-
+    public Animator animator;
     private void fall()
     {
-        //anim
+        animator.SetBool("deadtarget", true);
     }
+    private void Update()
+    {
+        if (HP < 0)
+        {
+            fall();
+        }
+    }
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Bullet"))
+        {
+            HP = HP - Bullet.Damage;
+            Debug.Log("gdfgdfgd");
+        }
 
+
+    }
 }
