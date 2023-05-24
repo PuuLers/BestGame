54b8838f980437d92669c6ea6191df224017a002
cans1z <canss1zzz@gmail.com>
2023-05-24 15:41:44 +0300

    enemy

--- BG/Assets/Scripts/Mouse.cs
+++ BG/Assets/Scripts/Mouse.cs
@@ -14,40 +14,11 @@ public class Mouse : MonoBehaviour
 
     void Start()
     {
-        rb = GetComponent<Rigidbody2D>();
+        player  = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
     }
 
     void Update()
     {
-        float distToPlayer = Vector2.Distance(transform.position, player.position);
-
-        if (distToPlayer < agrodistance) 
-        {
-            StartHunting();
-        }
-        else 
-        {
-            StopHunting();
-        }
-
-        void StartHunting()
-        {
-            if (player.position.x < transform.position.x || player.position.y < transform.position.y)
-            {
-                rb.velocity = new Vector2(speed, -speed);
-            }
-            else
-            {
-                rb.velocity = new Vector2(-speed, speed);
-            }
-        }
-
-        void StopHunting()
-        {
-            rb.velocity = new Vector2(0,0);
-        }
-
-
-
+        transform.position = Vector2.MoveTowards(transform.position, player.position, speed * Time.fixedDeltaTime);
     }
 } 
