6c868abd4e815bf84ed4af768935979315091d85
PuuLers <115350562+PuuLers@users.noreply.github.com>
2023-06-03 20:09:06 +0500

    Generator Save

---
+++ BG/Assets/Scripts/WallsGenerator.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallsGeneratorCell
+{
+    public int X = 20;
+    public int Y = 20;
+
+    public bool WallLeftl = true;
+    public bool WallDown = true;
+}
+
+
+
+public class WallsGenerator 
+{
+    public int Width;
+    public int Heigth;
+
+    public WallsGeneratorCell[,] Generate()
+    {
+        WallsGeneratorCell[,] Walls = new WallsGeneratorCell[Width, Heigth];
+
+        for (int x = 0; x < Walls.GetLength(0); x++)
+        {
+            for (int y = 0; y < Walls.GetLength(0); y++)
+            {
+                Walls[x, y] = new WallsGeneratorCell { X = x, Y = y };
+            }
+
+        }
+
+
+
+        return Walls;
+    }
+}
