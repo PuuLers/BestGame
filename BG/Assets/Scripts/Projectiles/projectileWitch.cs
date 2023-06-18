446e63fe773969a60dfb7ccbd2d6d5890dea1b8c
cans1z <canss1zzz@gmail.com>
2023-06-18 15:44:06 +0800

    fixxx

--- BG/Assets/Scripts/Projectiles/projectileWitch.cs
+++ BG/Assets/Scripts/Projectiles/projectileWitch.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class projectile : MonoBehaviour
+public class projectileWitch : MonoBehaviour
 {
     public float Speed;
     public int Damage;
