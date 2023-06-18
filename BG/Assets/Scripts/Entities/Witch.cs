fdc2216ddbe4fdd1f75bb9771b8f386912d9d296
cans1z <canss1zzz@gmail.com>
2023-06-18 15:39:22 +0800

    witch demo

---
+++ BG/Assets/Scripts/Entities/Witch.cs
@@ -0,0 +1,114 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Witch : MonoBehaviour
+{
+
+    public int Damage = 20;
+    public int HP = 50;
+    private Transform player;
+    public float speed;
+    public float agrodistance;
+    private float distance;
+    private Animator animator;
+    private bool AgroMode = false;
+    public float startTimeBtwShots;
+    public GameObject projectileWitch;
+    private float fireCooldown = 0.6f;
+    public float bulletSpeed;
+    public Transform shotPoint;
+    private bool isAttacking = false;
+    public GameObject bat;
+    public int batCount;
+
+    public void TakeDamage(int Damage)
+    {
+        HP -= Damage;
+        for (int i = 0; i < batCount; i++) 
+        {
+            Instantiate(bat);
+        }
+    }
+
+
+    private void Move()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, player.position, speed * Time.fixedDeltaTime);
+        Vector3 LocalScale = Vector3.one;
+        if (transform.position.x > player.position.x)
+        {
+            LocalScale.x = LocalScale.x * -1;
+        }
+        else
+        {
+            LocalScale.x = LocalScale.x * 1;
+        }
+        transform.localScale = LocalScale;
+    }
+
+
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+        player = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+    }
+
+
+    private void Update()
+    {
+        DistanceCheck();
+
+        if (AgroMode == true)
+        {
+            Move();
+
+
+
+            if (!isAttacking)
+            {
+                StartCoroutine(AttackDelay());
+            }
+            IEnumerator AttackDelay()
+            {
+                isAttacking = true;
+                yield return new WaitForSeconds(fireCooldown);
+                LrAttack();
+                isAttacking = false;
+            }
+        }
+        if (HP <= 0)
+        {
+            //animator.SetInteger("Slug states", 1);
+            AgroMode = false;
+            agrodistance = 0;
+        }
+    }
+
+   
+
+    public void LrAttack()
+    {
+        Vector2 direction = player.position - transform.position;
+        shotPoint.right = direction;
+        GameObject bullet = Instantiate(projectileWitch, shotPoint.position, Quaternion.identity);
+        bullet.transform.right = direction;
+        Rigidbody2D bulletRigidbody = bullet.GetComponent<Rigidbody2D>();
+        bulletRigidbody.velocity = direction.normalized * bulletSpeed;
+    }
+
+ 
+    public void DistanceCheck()
+    {
+        distance = Vector3.Distance(player.position, transform.position);
+        if (distance < agrodistance)
+        {
+            AgroMode = true;
+        }
+        else
+        {
+            AgroMode = false;
+        }
+    }
+}
---
+++ BG/Assets/Scripts/Projectiles/projectileWitch.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class projectile : MonoBehaviour
+{
+    public float Speed;
+    public int Damage;
+    public LayerMask WhatIsPlayer;
+    public float distanse;
+    public GameObject Witch;
+    private Transform player;
+    //private float projectileLifetime = 2f;
+
+
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+    }
+
+    void Update()
+    {
+        RaycastHit2D hitinfo = Physics2D.Raycast(transform.position, transform.right, distanse, WhatIsPlayer);
+        if (hitinfo.collider != null)
+        {
+            if (hitinfo.collider.CompareTag("Player"))
+            {
+                hitinfo.collider.GetComponent<Player>().TakeDamage(Damage);
+            }
+            Destroy(gameObject);
+        }
+        transform.Translate(Vector2.right * Speed * Time.deltaTime);
+        transform.position = Vector2.MoveTowards(transform.position, player.position, Speed * Time.deltaTime);
+
+    }
+}
+    
